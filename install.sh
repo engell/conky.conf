39b48cf878506ea342fe0c1454dd3793883fe818
engell <dark.engell.25@gmail.com>
2017-08-18 12:30:56 -0700

    Install script mkdir Fonts

--- install.sh
+++ install.sh
@@ -2,5 +2,6 @@
 cd ~/
 git clone https://github.com/engell/conky.conf.git
 mv ~/conky.conf ~/.conky
+mkdir ~/.local/share/fonts
 cp -rf ~/.conky/fonts/* ~/.local/share/fonts
 ln -s ~/.conky/conkyrc_engell ~/.conkyrc
